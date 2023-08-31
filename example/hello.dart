ad9101895683315f6ad2594c55c735588f49c2f2
Afdo Agustian <afdo.agustian@gmail.com>
2023-08-31 17:01:38 +0700

    Dart Library

---
+++ example/hello.dart
@@ -0,0 +1,10 @@
+import 'package:dart_library/hello.dart';
+
+void main(){
+  print(sayHello('Eko'));
+
+
+  var customer = Customer();
+  var product = Product();
+  var category = Category();
+}
---
+++ lib/hello.dart
@@ -0,0 +1,5 @@
+library dart_library;
+
+export 'src/say_hello.dart' show sayHello;
+export 'src/customer.dart' show Customer, Product, Category;
+
